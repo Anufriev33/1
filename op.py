b0aab8a3484a39447979b2bde5fb513345d29e89
roman <roma.anufriev.90@mail.ru>
2023-05-24 21:40:05 +0300

    https://github.com/Anufriev33/a-new-story.git

---
+++ op.py
@@ -0,0 +1,67 @@
+
+# a=5
+# b=5.89
+# c='privet'
+# print(f"{a} - {b} - {c}")
+# print("{}-{}-{}".format(a,b,c)   )
+# print("Введите первое число :")
+# a =int (input())
+# b= int (input('Введите второе число:'))
+# print(a, '+', b, '=', a b) 
+
+# a=5.4444
+# b=5.6666
+# print(round (a*b,2))
+# username=input('Введите имя:')
+# if username == 'ання':
+#     print('**Я тебя люблю!хе')
+# elif username == 'рома':
+#     print('Как дела?')
+# elif username =='mari':
+#     print('наша дочка*')
+# else:
+#     print('Привет' , 'друг')    
+# a=4.434343
+# b=6.45454545454
+# print (int(a*b ))
+# iter 
+# a=4>5and 5>9
+# print(a)
+
+
+# c=1
+# print(c)
+# print(type(c))
+# c=bool(c)
+# print(c)
+# print(type(c))
+# a = 5
+# b=4.44
+# c='hello'
+# print(f"{a},{b},{c}")
+# i = 0
+# while i < 5:
+    
+#      i = i + 1
+# else:
+#      print('Пожалуй')
+#      print('Хватит')
+# print(i)
+# a = 'adsdsds'
+# for i in a:
+#     print(a)
+# text = 'привет как тебя зовут'
+print(len(text))
+print('как' in text)
+
+
+
+a =5
+b =5.89
+c='privet'
+print(f"{a} - {b} - {c}")
+print("{}-{}-{}".format(a,b,c)   )
+print("Введите первое число :")
+a =int (input())
+b= int (input('Введите второе число:'))
+print( a, '+', b, '=', a b) 
